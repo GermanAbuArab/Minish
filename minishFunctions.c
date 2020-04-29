6a44806a0af0051a224f2d1444fd5da2975acdb5
Germán <gabuarab@correo.um.edu.uy>
2020-04-29 10:58:01 -0300

    funciones.c

--- minishFunctions.c
+++ minishFunctions.c
@@ -19,4 +19,63 @@
 #include <fcntl.h>
 #include "minish.h"
 
+int builtin_exit (int argc, char ** argv){
+
+}
+
+int builtin_help (int argc, char ** argv){
+
+}
+
+int builtin_history (int argc, char ** argv){
+
+}
+
+int builtin_status (int argc, char ** argv){
+
+}
+
+int builtin_cd (int argc, char ** argv){
+
+}
+
+int builtin_dir (int argc, char ** argv){
+
+}
+
+int builtin_getenv (int argc, char ** argv){
+
+}
+
+int builtin_gid (int argc, char ** argv){
+
+}
+
+int builtin_setenv (int argc, char ** argv){
+
+}
+
+int builtin_pid (int argc, char ** argv){
+
+}
+
+int builtin_uid (int argc, char ** argv){
+
+}
+
+int builtin_unsetenv (int argc, char ** argv){
+
+}
+
+int ejecutar (int argc, char ** argv){
+
+}
+
+int externo (int argc, char ** argv){
+
+}
+
+int linea2argv(char *linea, int argc, char **argv){
+
+}
 
