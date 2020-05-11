41062a0201cc536eb255fc1dbde1072902cb2a1e
Germán <gabuarab@correo.um.edu.uy>
2020-05-11 11:13:55 -0300

    cambio de cd.2

--- builtin_cd.c
+++ builtin_cd.c
@@ -2,19 +2,24 @@
 
 
 int builtin_cd(int argc, char **argv) {
-    if (argv[1] == "-") {
+    if (argc>1) {
+        if (strcmp(argv[1],"-")==0) {
 
-
-    } else if (argv[1] == "--") {
-        if (chdir(getenv("HOME")) != 0) { return -1; }
-        else { return 0; }
-    } else {
-        if (chdir(argv[1]) != 0) {
-            printf("No existe esa carpeta o directorio \n");
-            return -1;
-        } else {
             return 0;
+        } else if (strcmp(argv[1],"--")==0) {
+            if (chdir(getenv("HOME")) != 0) { return -1; }
+            else { return 0; }
+        } else {
+            if (chdir(argv[1]) != 0) {
+                printf("No existe esa carpeta o directorio \n");
+                return -1;
+            } else {
+                return 0;
+            }
         }
+    } else {
+        printf("Tiene que dar una directoria al cual ir");
+        return -1;
     }
 }
 
