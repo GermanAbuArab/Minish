98b12f7ceb1b16cf03a63dd9ff6a75290177a3c7
TEJERÍA OLASO <ftejeria@VM-FIUM-SO.agt1aeotuneepp23yebz0qxtbb.cx.internal.cloudapp.net>
2020-05-16 18:34:54 -0300

    chau control c y heap memory leaks

--- builtin_gid.c
+++ builtin_gid.c
@@ -11,8 +11,10 @@ int builtin_gid(__attribute__((unused))int argc,__attribute__((unused)) char **a
     }
     if ((group = malloc(nogroups * sizeof(gid_t))) == NULL) {
         printf("Error interno de Memoria \n");
+        free(group);
         return -1;
     }
+
     if (getgroups(nogroups, group) !=-1) {
         printf("Id de los grupos secundarios:");
         for (int i = 0; i < nogroups; ++i) {
@@ -23,8 +25,10 @@ int builtin_gid(__attribute__((unused))int argc,__attribute__((unused)) char **a
         printf("\n");
     } else {
 
-//todo handelear errores
+       free(group);
         return -1;
     }
+    free(group);
     return 0;
 }
+
--- minish.c
+++ minish.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <signal.h>
 
 #include "minish.h"
 #define MAXLINE  1024        // tamaño máximo de la línea de entrada
@@ -11,6 +11,14 @@
 int globalstatret;
 char *oldpwd=NULL;
 char *cwd =NULL;
+int argc;
+
+
+    void signal_handler(int sig){
+        printf(" Signal number: %d\n", sig);
+        argc = 0;
+        return;
+    }
 
 int main() {
     FILE *historial;
@@ -18,26 +26,50 @@ int main() {
     char dirHist[MAXLINE];
     int argc = MAXWORDS;
     char *argv[MAXWORDS];
+    struct sigaction new_action;
+    new_action.sa_handler = signal_handler;
+    snprintf(dirHist, MAXLINE, "%s/%s",getenv("HOME"),HISTORY_FILE);
+    historial=fopen(dirHist,"a");
 
     while(globalstatret!=10) {
+
+         if (sigaction(SIGINT, &new_action, NULL) == -1) {
+                continue;   
+                }   
+       
+
+        
+       // sigaction(SIGINT, &new_action, NULL);
+        //------------------------------///
         fprintf(stderr, "(minish)%s:%s>", getenv("USER"),getcwd(str,100));
-        cwd=strdup(str);
-        snprintf(dirHist, MAXLINE, "%s/%s",getenv("HOME"),HISTORY_FILE);
+        cwd=malloc(strlen(str)+1);
+        strcpy(cwd,str);
         fgets(str,MAXLINE,stdin);
         if(str!=NULL) {
+
+
             if(strcmp(str,"\n")!=0) {
-                historial=fopen(dirHist,"a");
                 argc =linea2argv(str,argc,argv);
+                if (strcmp(argv[0],"exit")==0)
+                {
+                    fclose(historial);
+                    free(cwd);
+                }
                 globalstatret=ejecutar(argc,argv);
                 fprintf(historial,"%s\n",argv[0]);
-                fclose(historial);
+
+            
+        
             }
         }
+
         else {
             // todo handelear error de strcmp
         }
+        free(cwd);
     }
+    
+
 
 }
-
 
